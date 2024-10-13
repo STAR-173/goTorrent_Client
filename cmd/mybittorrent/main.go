b9aefcf6d057d37685a0122a85509e625a21f496
Shoaib Akhtar <starvoltz173@gmail.com>
2024-10-13 20:57:16 +0530

    implemented bencoded list decoding

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"os"
+
+	"github.com/jackpal/bencode-go"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
@@ -15,117 +17,6 @@ var _ = json.Marshal
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
 
-func decodeString(b string, st int) (x interface{}, i int, err error) {
-	var l int
-
-	i = st
-
-	for i < len(b) && b[i] >= '0' && b[i] <= '9' {
-		l = l*10 + (int(b[i]) - '0')
-		i++
-	}
-	if i == len(b) || b[i] != ':' {
-		return "", st, fmt.Errorf("Bad String")
-	}
-
-	i++
-
-	if i+1 > len(b) {
-		return "", st, fmt.Errorf("Bad String")
-	}
-
-	x = b[i : i+l]
-	i += l
-
-	return x, i, nil
-}
-
-func decodeInt(b string, st int) (x int, i int, err error) {
-	i = st
-	i++
-
-	if i == len(b) {
-		return 0, st, fmt.Errorf("Bad Int")
-	}
-
-	neg := false
-
-	if b[i] == '-' {
-
-		neg = true
-
-		i++
-
-	}
-
-	for i < len(b) && b[i] >= '0' && b[i] <= '9' {
-
-		x = x*10 + (int(b[i]) - '0')
-
-		i++
-
-	}
-
-	if i == len(b) || b[i] != 'e' {
-		return 0, st, fmt.Errorf("Bad Int")
-	}
-
-	i++
-
-	if neg {
-		x = -x
-	}
-
-	return x, i, nil
-}
-
-func decodeList(b string, st int) (l []interface{}, i int, err error) {
-	i = st
-	i++
-
-	l = make([]interface{}, 0)
-
-	for {
-		if i >= len(b) {
-			return nil, st, fmt.Errorf("Bad List")
-		}
-
-		if b[i] == 'e' {
-			break
-		}
-
-		var x interface{}
-
-		x, i, err = decode(b, i)
-		if err != nil {
-			return nil, i, err
-		}
-
-		l = append(l, x)
-	}
-
-	return l, i, nil
-}
-
-func decode(b string, st int) (x interface{}, i int, err error) {
-	if st == len(b) {
-		return nil, st, io.ErrUnexpectedEOF
-	}
-
-	i = st
-
-	switch {
-	case b[0] == 'i':
-		return decodeInt(b, i)
-	case b[i] == 'l':
-		return decodeList(b, i)
-	case b[i] >= '0' && b[i] <= '9':
-		return decodeString(b, i)
-	default:
-		return nil, st, fmt.Errorf("unexpected value %q", b[i])
-	}
-}
-
 func main() {
 
 	command := os.Args[1]
@@ -135,7 +26,7 @@ func main() {
 
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, err := bencode.Decode(bytes.NewReader([]byte(bencodedValue)))
 		if err != nil {
 			fmt.Println(err)
 			return
