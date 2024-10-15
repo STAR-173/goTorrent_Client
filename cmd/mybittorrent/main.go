e478a4da39ff143682f1a17954a06306fe1b290a
Shoaib Akhtar <starvoltz173@gmail.com>
2024-10-15 18:40:15 +0530

    implemented pieces hash print info

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,79 +1,199 @@
 package main
 
 import (
+
+	// Uncomment this line to pass the first stage
+
+	// "encoding/json"
+
 	"bytes"
+
 	"crypto/sha1"
+
+	"encoding/hex"
+
 	"encoding/json"
+
 	"fmt"
+
 	"os"
 
-	"github.com/jackpal/bencode-go"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
+	"strings"
+
+	bencode "github.com/jackpal/bencode-go"
 )
 
-// Ensures gofmt doesn't remove the "os" encoding/json import (feel free to remove this!)
-var _ = json.Marshal
+type TorrentMetaInfo struct {
+	Length int `bencode:"length"`
 
-type Metafile struct {
-	Announce string   `bencode:"announce"`
-	Info     Metainfo `bencode:"info"`
+	Name string `bencode:"name"`
+
+	PieceLength int `bencode:"piece length"`
+
+	Pieces string `bencode:"pieces"`
 }
 
-type Metainfo struct {
-	PieceLength int    `bencode:"piece length"`
-	Pieces      string `bencode:"pieces"`
-	Length      int    `bencode:"length"`
-	Name        string `bencode:"name"`
+type TorrentFile struct {
+	Announce string `bencode:"announce"`
+
+	CreatedBy string `bencode:"created by"`
+
+	Info TorrentMetaInfo `bencode:"info"`
 }
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
+type Torrent struct {
+	Announce string
+
+	Name string
+
+	Length int
+
+	InfoHash [20]byte
+
+	PieceLength int
+
+	PieceHashes [][20]byte
+}
+
+func (tr *TorrentFile) toTorrent() Torrent {
+
+	infoHash := tr.Info.hash()
+
+	pieceHashes := tr.Info.pieceHashes()
+
+	return Torrent{
+
+		Announce: tr.Announce,
+
+		Name: tr.Info.Name,
+
+		Length: tr.Info.Length,
+
+		InfoHash: infoHash,
+
+		PieceHashes: pieceHashes,
+
+		PieceLength: tr.Info.PieceLength,
+	}
+
+}
+
+func (meta *TorrentMetaInfo) hash() [20]byte {
+
+	fmt.Println(meta.Pieces)
+
+	sha := sha1.New()
+
+	bencode.Marshal(sha, *meta)
+
+	h := sha.Sum(nil)
+
+	// fmt.Println(h)
+
+	var asd [20]byte
+
+	copy(asd[:], h[:20])
+
+	// fmt.Println(asd)
+
+	return asd
+
+}
+
+func (meta *TorrentMetaInfo) pieceHashes() [][20]byte {
+
+	hashLen := 20
+
+	buf := []byte(meta.Pieces)
+
+	numHashes := len(buf) / hashLen
+
+	hashes := make([][20]byte, numHashes)
+
+	for i := 0; i < numHashes; i++ {
+
+		copy(hashes[i][:], buf[i*hashLen:(i+1)*hashLen])
+
+	}
+
+	return hashes
+
+}
 
 func main() {
 
 	command := os.Args[1]
 
 	switch command {
+
 	case "decode":
-		// Uncomment this block to pass the first stage
 
 		bencodedValue := os.Args[2]
 
-		decoded, err := bencode.Decode(bytes.NewReader([]byte(bencodedValue)))
+		decoded, err := bencode.Decode(strings.NewReader(bencodedValue))
+
 		if err != nil {
+
 			fmt.Println(err)
+
 			return
+
 		}
 
 		jsonOutput, _ := json.Marshal(decoded)
+
 		fmt.Println(string(jsonOutput))
 
 	case "info":
-		inputFile := os.Args[2]
-		contents, err := os.ReadFile(inputFile)
 
-		var result Metafile
+		filename := os.Args[2]
 
-		err = bencode.Unmarshal(bytes.NewReader([]byte(contents)), &result)
+		content, err := os.ReadFile(filename)
+
 		if err != nil {
 
-			fmt.Println(err)
+			fmt.Println("Error reading file content:", err)
 
-			return
+			os.Exit(1)
 
 		}
 
-		h := sha1.New()
+		reader := bytes.NewReader(content)
 
-		bencode.Marshal(h, result.Info)
+		var meta TorrentFile
 
-		fmt.Printf("Tracker URL: %v\n", result.Announce)
-		fmt.Printf("Length: %v\n", result.Info.Length)
-		fmt.Printf("Info Hash: %x\n", h.Sum(nil))
+		err = bencode.Unmarshal(reader, &meta)
+
+		if err != nil {
+
+			fmt.Println("Error decoding file content:", err)
+
+			os.Exit(1)
+
+		}
+
+		torrent := meta.toTorrent()
+
+		fmt.Printf("Tracker URL: %s\n", torrent.Announce)
+
+		fmt.Printf("Length: %v\n", torrent.Length)
+
+		fmt.Printf("Info Hash: %s\n", hex.EncodeToString(torrent.InfoHash[:]))
+
+		fmt.Printf("Piece Length: %d\n", torrent.PieceLength)
+
+		for i := 0; i < len(torrent.PieceHashes); i++ {
+
+			fmt.Printf("%s\n", hex.EncodeToString(torrent.PieceHashes[i][:]))
+
+		}
 
 	default:
+
 		fmt.Println("Unknown command: " + command)
+
 		os.Exit(1)
+
 	}
+
 }
